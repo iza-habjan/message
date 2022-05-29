76d198c417c99696d41ba941f4128b1c5a5e4cd8
Iza Habjan <iza.habjan@gmail.com>
2022-05-29 12:54:29 +0200

    encoding the entered string

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ document.querySelector("form").addEventListener("submit", (event) => {
   // to prevent default browser action of submiting the form to some backend server
   event.preventDefault();
 
-  const input = document.querySelector("input");
-  console.log(input.value);
+  const input = document.querySelector("#message-input");
+  const encrypted = btoa(input.value);
+
+  document.querySelector("#link-input").value = encrypted;
 });
