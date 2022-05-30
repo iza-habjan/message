3f0b7925ee20438622ad277b7e02b7cc85d91616
Iza Habjan <iza.habjan@gmail.com>
2022-05-30 08:51:50 +0200

    toggling element visibility

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ document.querySelector("form").addEventListener("submit", (event) => {
   // to prevent default browser action of submiting the form to some backend server
   event.preventDefault();
 
+  // toggle element visibility
+  document.querySelector("#message-form").classList.add("hide");
+  document.querySelector("#link-form").classList.remove("hide");
+
   const input = document.querySelector("#message-input");
   const encrypted = btoa(input.value);
 
